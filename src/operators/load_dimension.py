9e46f0f5725542adab9c5c8082e92e8e2708c1d6
HarrisonHenri <harrisonhenrisn@gmail.com>
2023-09-09 16:54:39 -0300

    fix: minor fixes

--- src/operators/load_dimension.py
+++ src/operators/load_dimension.py
@@ -13,22 +13,21 @@ class LoadDimensionalOperator(BaseOperator):
         self,
         sql_query="",
         redshift_conn_id="redshift",
-        delete_table: Optional[str] = None,
+        truncate_table: Optional[str] = None,
         *args,
         **kwargs,
     ):
         super(LoadDimensionalOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.sql_query = sql_query
-        self.delete_table = delete_table
+        self.truncate_table = truncate_table
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
-        if self.delete_table:
-            self.log.info(f"Cleaning table {self.delete_table}")
-            redshift.run("DELETE FROM {}".format(self.delete_table))
-            return
+        if self.truncate_table:
+            self.log.info(f"Truncating table {self.truncate_table}")
+            redshift.run("TRUNCATE TABLE {}".format(self.truncate_table))
 
         self.log.info("Starting dimensional table insert")
         redshift.run(self.sql_query)
